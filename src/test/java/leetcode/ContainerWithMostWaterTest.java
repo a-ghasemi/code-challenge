848c82f57c8b66838b819688aacc9192bdb1c724
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-25 13:59:12 +0100

    ContainerWithMostWater: add new test case

--- src/test/java/leetcode/ContainerWithMostWaterTest.java
+++ src/test/java/leetcode/ContainerWithMostWaterTest.java
@@ -8,9 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ContainerWithMostWaterTest {
 
     @Test
-    void maxArea() {
+    void maxArea1() {
         int[] input = new int[]{1,8,6,2,5,4,8,3,7};
         int expectedOutput = 49;
+
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+
+        Assertions.assertEquals(expectedOutput, containerWithMostWater.maxArea(input));
+    }
+
+    @Test
+    void maxArea2() {
+        int[] input = new int[]{1,1};
+        int expectedOutput = 1;
 
         ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
 
