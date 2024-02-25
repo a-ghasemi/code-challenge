0f5419725ccdad68af7a20b5398e8849e2e48a62
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-25 14:46:45 +0100

    CountPairsWithGivenSum: naive solution

--- src/main/java/geeksforgeeks/CountPairsWithGivenSum.java
+++ src/main/java/geeksforgeeks/CountPairsWithGivenSum.java
@@ -4,7 +4,15 @@ package geeksforgeeks;
 public class CountPairsWithGivenSum {
 
     public int countPairsWithExactSum(int[] numbers, int k){
-        return 1;
+        int count = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                if(numbers[i]+numbers[j] == k) count++;
+            }
+        }
+
+        return count;
     }
 
 }
