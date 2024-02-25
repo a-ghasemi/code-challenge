95dfc75962dec798b32d052ac4b1f892190e11dc
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-25 14:22:37 +0100

    ContainerWithMostWater: change to greedy algorithm for big test

--- src/main/java/leetcode/ContainerWithMostWater.java
+++ src/main/java/leetcode/ContainerWithMostWater.java
@@ -5,11 +5,19 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height){
         int maxArea = 0;
 
-        for(int i=0; i < height.length - 1; i++){
-            for(int j=i+1; j < height.length; j++){
-                maxArea = Math.max(maxArea, calculateArea(height, i, j));
+        int left = 0;
+        int right = height.length - 1;
+
+        do {
+            maxArea = Math.max(maxArea, calculateArea(height, left, right));
+            if(height[left] < height[right]){
+                left += 1;
             }
-        }
+            else{
+                right -= 1;
+            }
+
+        } while(left < right);
 
         return maxArea;
     }
