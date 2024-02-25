8fb05ae2e9b5d6ebe0ac07fce91fd0e8b826e4e4
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-25 13:59:00 +0100

    ContainerWithMostWater: add solution

--- src/main/java/leetcode/ContainerWithMostWater.java
+++ src/main/java/leetcode/ContainerWithMostWater.java
@@ -3,7 +3,17 @@ package leetcode;
 // https://leetcode.com/problems/container-with-most-water
 public class ContainerWithMostWater {
     public int maxArea(int[] height){
-        return 1;
+        int maxArea = 0;
+
+        for(int i=0; i < height.length - 1; i++){
+            for(int j=i+1; j < height.length; j++){
+                int h = Math.min(height[i],height[j]);
+                int a = h * (j - i);
+                maxArea = Math.max(maxArea, a);
+            }
+        }
+
+        return maxArea;
     }
 
 }
