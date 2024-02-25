d57b4da8710d9fc081f952215317cf779f162d46
Armin Ghasemi <armin.ghassemi@gmail.com>
2024-02-25 14:33:14 +0100

    ContainerWithMostWater: keep bruteforce solution to check more possibilities in the future

--- src/main/java/leetcode/ContainerWithMostWater.java
+++ src/main/java/leetcode/ContainerWithMostWater.java
@@ -22,9 +22,28 @@ public class ContainerWithMostWater {
         return maxArea;
     }
 
+    // checks every possibility to calculate the expected answer for test cases
+    public int bruteforceMaxArea(int[] height){
+        int maxArea = 0;
+
+        int left = 0;
+        int right = height.length - 1;
+
+        do {
+            maxArea = Math.max(maxArea, calculateArea(height, left, right));
+            if(height[left] < height[right]){
+                left += 1;
+            }
+            else{
+                right -= 1;
+            }
+
+        } while(left < right);
+
+        return maxArea;
+    }
     private static int calculateArea(int[] height, int i, int j) {
         return Math.min(height[i], height[j]) * (j - i);
     }
-
 }
 
---
+++ src/main/java/leetcode/readme.md
@@ -0,0 +1,6 @@
+# Container with most water
+url: https://leetcode.com/problems/container-with-most-water
+
+## ToDo:
+* I guess the greedy algorithm can retrieve wrong answer in some cases. then I keep the bruteforce answer model to check some possibilities in the futuer.
+* I prefer to use Factory Method Pattern and add some more solutions. like a Genetic Algorithm solution for this challenge"
