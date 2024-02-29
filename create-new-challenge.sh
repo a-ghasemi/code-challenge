8f8d30587621b6f0f7c5740481be351c84b8d757
Armin Ghasemi <armin.ghasemi@portavita.nl>
2024-02-29 14:53:40 +0100

    Generator: third try

--- create-new-challenge.sh
+++ create-new-challenge.sh
@@ -22,26 +22,23 @@ mkdir -p "$TEST_PLATFORM_PATH"
 cp -r "$SRC_PATH/$TEMPLATE_PACKAGE/Template" "$PLATFORM_PATH/$CHALLENGE_NAME"
 cp -r "$TEST_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" # Adjusted to copy directly to a new challenge-specific directory
 
-# Function to handle sed in-place editing for compatibility
-function sed_inplace() {
-    if [[ "$(uname)" == "Darwin" ]]; then
-        sed -i '' "$@"
-    else
-        sed -i "$@"
-    fi
-}
+# Determine the correct sed command based on the operating system
+SED_CMD="sed -i"
+if [[ "$(uname)" == "Darwin" ]]; then
+    SED_CMD="sed -i ''"
+fi
 
 # Rename package in Java files
-find "$PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/package templatepackage.Template;/package '"$PLATFORM"'.'"$CHALLENGE_NAME"';/g" "$1"' _ {} \;
+find "$PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/package templatepackage.Template;/package $PLATFORM.$CHALLENGE_NAME;/g" {} \;
 
 # Update README.md within the challenge directory, checking if the file exists
 README_PATH="$PLATFORM_PATH/$CHALLENGE_NAME/README.md"
 if [[ -f "$README_PATH" ]]; then
-    sed_inplace "s/# Template Title/# $CHALLENGE_NAME/g" "$README_PATH"
+    $SED_CMD "s/# Template Title/# $CHALLENGE_NAME/g" "$README_PATH"
 fi
 
 # Update test case package and import statements
-find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/package templatepackage;/package '"$PLATFORM"';/g" "$1"' _ {} \;
-find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/import templatepackage.Template.*;/import '"$PLATFORM"'.'"$CHALLENGE_NAME"';/g" "$1"' _ {} \;
+find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/package templatepackage;/package $PLATFORM;/g" {} \;
+find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/import templatepackage.Template.*;/import $PLATFORM.$CHALLENGE_NAME.*;/g" {} \;
 
 echo "Challenge $CHALLENGE_NAME setup completed in platform $PLATFORM."
