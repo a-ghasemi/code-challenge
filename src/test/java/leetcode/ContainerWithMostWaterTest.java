e9fd1f0e5f5f028cb9affed81f3eb63eb95818ad
Armin Ghasemi <armin.ghasemi@portavita.nl>
2024-02-29 14:07:12 +0100

    ContainerWithMostWater: implement parametrized test case

--- src/test/java/leetcode/ContainerWithMostWaterTest.java
+++ src/test/java/leetcode/ContainerWithMostWaterTest.java
@@ -2,25 +2,32 @@ package leetcode;
 
 import leetcode.ContainerWithMostWater.*;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class ContainerWithMostWaterTest {
 
-    @Test
-    void maxArea1() {
-        int[] input = new int[]{1,8,6,2,5,4,8,3,7};
-        int expectedOutput = 49;
+    static Strategy strategy = new BruteforceStrategy();
 
-        SolutionExecutor solutionExecutor = new SolutionExecutor(new BruteforceStrategy());
-        Assertions.assertEquals(expectedOutput, solutionExecutor.execute(input));
+    static Stream<Object[]> dataProvider(){
+        return Stream.of(
+                new Object[]{
+                        new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7},
+                        49
+                },
+                new Object[]{
+                        new int[]{1, 1},
+                        1
+                }
+        );
     }
 
-    @Test
-    void maxArea2() {
-        int[] input = new int[]{1,1};
-        int expectedOutput = 1;
-
-        SolutionExecutor solutionExecutor = new SolutionExecutor(new BruteforceStrategy());
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void maxArea(int[] input, int expectedOutput) {
+        SolutionExecutor solutionExecutor = new SolutionExecutor(strategy);
         Assertions.assertEquals(expectedOutput, solutionExecutor.execute(input));
     }
 }
