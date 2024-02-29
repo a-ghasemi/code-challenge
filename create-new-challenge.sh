d748982ed5771ae9c9efec677673bec10bd670ac
Armin Ghasemi <armin.ghasemi@portavita.nl>
2024-02-29 15:03:33 +0100

    Generator: bugfix: remove middle category in test

--- create-new-challenge.sh
+++ create-new-challenge.sh
@@ -12,6 +12,7 @@ SRC_PATH="src/main/java"
 TEST_PATH="src/test/java"
 TEMPLATE_PACKAGE="templatepackage"
 PLATFORM_PATH="$SRC_PATH/$PLATFORM"
+README_PATH="$PLATFORM_PATH/$CHALLENGE_NAME/readme.md"
 TEST_PLATFORM_PATH="$TEST_PATH/$PLATFORM"
 
 # Ensure the correct directory structure
@@ -20,7 +21,7 @@ mkdir -p "$TEST_PLATFORM_PATH"
 
 # Copy template package to new challenge package
 cp -r "$SRC_PATH/$TEMPLATE_PACKAGE/Template" "$PLATFORM_PATH/$CHALLENGE_NAME"
-cp -r "$TEST_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" # Adjusted to copy directly to a new challenge-specific directory
+cp -r "$TEST_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH"
 
 # Determine the correct sed command based on the operating system
 SED_CMD="sed -i"
@@ -32,13 +33,12 @@ fi
 find "$PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/package templatepackage.Template;/package $PLATFORM.$CHALLENGE_NAME;/g" {} \;
 
 # Update README.md within the challenge directory, checking if the file exists
-README_PATH="$PLATFORM_PATH/$CHALLENGE_NAME/README.md"
 if [[ -f "$README_PATH" ]]; then
     $SED_CMD "s/# Template Title/# $CHALLENGE_NAME/g" "$README_PATH"
 fi
 
 # Update test case package and import statements
-find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/package templatepackage;/package $PLATFORM;/g" {} \;
-find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec $SED_CMD "s/import templatepackage.Template.*;/import $PLATFORM.$CHALLENGE_NAME.*;/g" {} \;
+find "$TEST_PLATFORM_PATH" -type f -name "*.java" -exec $SED_CMD "s/package templatepackage;/package $PLATFORM;/g" {} \;
+find "$TEST_PLATFORM_PATH" -type f -name "*.java" -exec $SED_CMD "s/import templatepackage.Template.*;/import $PLATFORM.$CHALLENGE_NAME.*;/g" {} \;
 
 echo "Challenge $CHALLENGE_NAME setup completed in platform $PLATFORM."
