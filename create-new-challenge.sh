a3520f1b482ef12fb718a311de582b1bd5572667
Armin Ghasemi <armin.ghasemi@portavita.nl>
2024-02-29 14:51:11 +0100

    Generator: second try

--- create-new-challenge.sh
+++ create-new-challenge.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-PLATFORM=$1
+PLATFORM=$(echo "$1" | tr '[:upper:]' '[:lower:]') # Convert platform name to lowercase
 CHALLENGE_NAME=$2
 SRC_PATH="src/main/java"
 TEST_PATH="src/test/java"
@@ -14,25 +14,34 @@ TEMPLATE_PACKAGE="templatepackage"
 PLATFORM_PATH="$SRC_PATH/$PLATFORM"
 TEST_PLATFORM_PATH="$TEST_PATH/$PLATFORM"
 
-# Create or use existing platform package
+# Ensure the correct directory structure
 mkdir -p "$PLATFORM_PATH"
 mkdir -p "$TEST_PLATFORM_PATH"
 
 # Copy template package to new challenge package
 cp -r "$SRC_PATH/$TEMPLATE_PACKAGE/Template" "$PLATFORM_PATH/$CHALLENGE_NAME"
-cp -r "$TEST_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH"
+cp -r "$TEST_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" # Adjusted to copy directly to a new challenge-specific directory
+
+# Function to handle sed in-place editing for compatibility
+function sed_inplace() {
+    if [[ "$(uname)" == "Darwin" ]]; then
+        sed -i '' "$@"
+    else
+        sed -i "$@"
+    fi
+}
 
 # Rename package in Java files
-find "$PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec sed -i '' "s/package $TEMPLATE_PACKAGE.Template;/package $PLATFORM.$CHALLENGE_NAME;/g" {} \;
+find "$PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/package templatepackage.Template;/package '"$PLATFORM"'.'"$CHALLENGE_NAME"';/g" "$1"' _ {} \;
 
-# Update README.md within the challenge directory
-sed -i '' "s/# Template Title/# $CHALLENGE_NAME/g" "$PLATFORM_PATH/$CHALLENGE_NAME/README.md"
+# Update README.md within the challenge directory, checking if the file exists
+README_PATH="$PLATFORM_PATH/$CHALLENGE_NAME/README.md"
+if [[ -f "$README_PATH" ]]; then
+    sed_inplace "s/# Template Title/# $CHALLENGE_NAME/g" "$README_PATH"
+fi
 
 # Update test case package and import statements
-find "$TEST_PLATFORM_PATH/$TEMPLATE_PACKAGE" -type f -name "SolutionTest.java" -exec sed -i '' "s/package $TEMPLATE_PACKAGE;/package $PLATFORM;/g" {} \;
-find "$TEST_PLATFORM_PATH/$TEMPLATE_PACKAGE" -type f -name "SolutionTest.java" -exec sed -i '' "s/import $TEMPLATE_PACKAGE.Template.*;/import $PLATFORM.$CHALLENGE_NAME.*;/g" {} \;
-
-# Rename the template package directory in tests to match the challenge name
-mv "$TEST_PLATFORM_PATH/$TEMPLATE_PACKAGE" "$TEST_PLATFORM_PATH/$CHALLENGE_NAME"
+find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/package templatepackage;/package '"$PLATFORM"';/g" "$1"' _ {} \;
+find "$TEST_PLATFORM_PATH/$CHALLENGE_NAME" -type f -name "*.java" -exec bash -c 'sed_inplace "s/import templatepackage.Template.*;/import '"$PLATFORM"'.'"$CHALLENGE_NAME"';/g" "$1"' _ {} \;
 
 echo "Challenge $CHALLENGE_NAME setup completed in platform $PLATFORM."
