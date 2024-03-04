a80ff2daf42fce7ee884f2962de4155491da5d4e
Armin <datarivers.org@gmail.com>
2024-03-04 16:24:23 +0100

    EvenOddTree: map array to tree, works perfect with complete tree. but not with null in upper levels

--- src/main/java/leetcode/EvenOddTree/abstractions/TreeNode.java
+++ src/main/java/leetcode/EvenOddTree/abstractions/TreeNode.java
@@ -18,4 +18,17 @@ public class TreeNode {
         this.right = right;
     }
 
+    public static TreeNode fillByArray(Integer[] input) {
+        return fillByArray(input, 0);
+    }
+
+    public static TreeNode fillByArray(Integer[] input, int index) {
+        if (index >= input.length || input[index] == null) return null;
+
+        return new TreeNode(input[index],
+                fillByArray(input, 2 * index + 1),
+                fillByArray(input, 2 * index + 2)
+        );
+
+    }
 }
